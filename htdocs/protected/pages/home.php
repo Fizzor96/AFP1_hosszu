fdc56ba5f36e11a38ecb3521518aadbbda294140
Zsolti Berkes <zsoltiberkes2@gmail.com>
2020-11-17 17:25:00 +0100

    Home.php: csalogató, város kiválasztó hozzáadva

---
+++ htdocs/protected/pages/home.php
@@ -0,0 +1,23 @@
+<div class="container-fluid px-0">
+
+	<div class="background d-flex justify-content-center align-items-center flex-column ">
+
+		<div class="row">
+			<div class="col">
+				<h2 class="csalogato">Netán éhes vagy? Válaszd ki a városod és rendelj!</h2>
+			</div>
+		</div>
+
+
+		<div class="dropdown">
+			<button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Válassz várost
+			</button>
+			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<a class="dropdown-item" href="index.php?P=home&V=Eger">Eger</a>
+				<a class="dropdown-item" href="index.php?P=home&V=Budapest">Budapest</a>
+				<a class="dropdown-item" href="index.php?P=home&V=Szolnok">Szolnok</a>
+			</div>
+		</div>
+	</div>
+</div>

4df4ad121a8f55e7e16e0554c102645b14a51cf7
Zsolti Berkes <zsoltiberkes2@gmail.com>
2020-11-17 17:27:27 +0100

    Home.php: csalogató kibővítve, elérhetőségek hozzáadva

--- htdocs/protected/pages/home.php
+++ htdocs/protected/pages/home.php
@@ -21,3 +21,29 @@
 		</div>
 	</div>
 </div>
+<div class="szurke container-fluid d-flex align-items-center mt-10 ">
+	<div class="row flex-column flex-sm-column flex-md-column flex-lg-row flex-lx-row flex-wrap">
+		<div class="col d-flex "><img class="csalogatopizza mx-auto"src="<?=PUBLIC_DIR.'pizzabg.jpg'?>"></div>
+		<div class="col d-flex align-items-center">
+			<p class="csalogato2 text-center">
+				Rendeld meg az ételed online bankkártyás fizetéssel, és akár tiéd lehet a sok értékes nyeremény egyike. Emellett használd a kuponkódokat amikkel kedvezményeket vehetsz igénybe az éttermeknél! Ne maradj le, rendelj MOST!
+			</p>
+		</div>
+
+
+	</div>
+</div>
+
+<div class="kapcsolat container-fluid ">
+	<div class="row flex-column">
+		<div class="col">
+			<h3 class="text-center elerhetosegek">Elérhetőségek</h3>
+		</div>
+		<div class="col">
+			<p class="pkapcsolat text-center p-10">
+				Ha kérdésed lenne az oldallal vagy az éttermekkel nyugodtan keress minket telefonon vagy emailben!<br> Ügyfélszolgálatunk 0-24 elérhető és fél órán belül választ kaphatsz a kérdésedre!
+				
+			</p>	
+		</div>
+	</div>
+</div>

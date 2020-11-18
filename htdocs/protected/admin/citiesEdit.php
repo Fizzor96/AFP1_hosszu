3d80ddadbf9e26f14dc15debeb7fc1ad5534bda0
Fizzor <marwel96@gmail.com>
2020-11-18 14:07:10 +0100

    Admin - városok módosítása - backend

--- htdocs/protected/admin/citiesEdit.php
+++ htdocs/protected/admin/citiesEdit.php
@@ -1,3 +1,26 @@
+<?php
+require_once DATABASE_CONTROLLER;
+
+if(isset($_POST['edit'])):
+	$query = "UPDATE cities SET Name =:nev WHERE cid = :id";
+	$params = [ 
+		":nev" => $_POST["Name"],
+		":id" => $_POST["cid"]];
+	if(!executeDML($query,$params)):
+		echo 'Gebasz van<br>';
+		print_r($_POST);
+		echo '<br>';
+		print_r($params);
+	else:
+		header('Location: index.php?P=admin');
+	endif;
+
+else:
+	$query = "SELECT * FROM cities WHERE cid = :id";
+	$params = [':id' => $_GET['id']];
+	$d = getRecord($query,$params);
+	?>
+
 <div class="container-fluid p-4">
     <form method="POST">
         <input type="hidden" name="cid" value="<?=$d['cid']?>">
@@ -8,3 +31,4 @@
         <button name="edit" type="submit" class="btn btn-primary">Frissítés</button>
     </form>
 </div>
+<?php endif;?>

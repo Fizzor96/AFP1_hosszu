cdd3ffae1b5140c3aa291fe0bf00e49aaa6e46e3
Bence <egyjatekos0204@gmail.com>
2020-11-18 17:39:52 +0100

    Frontend: Felhasználó hozzáadása felület
    userAdd.php változtatva.

---
+++ htdocs/protected/admin/userAdd.php
@@ -0,0 +1,23 @@
+
+	<div class="container-fluid p-4">
+		<form method="POST">
+			<div class="form-group">
+				<label >Felhasználónév</label>
+				<input name="username" type="text" class="form-control">
+			</div>
+			<div class="form-group">
+				<label >Email</label>
+				<input name="email" type="email" class="form-control" >
+			</div>
+			<div class="form-group">
+				<label >Jelszó</label>
+				<input name="password" type="password" class="form-control" >
+			</div>
+			<div class="form-group">
+				<label >Jogok</label>
+				<input name="flags" type="text" class="form-control" >
+			</div>
+			<button name="edit" type="submit" class="btn btn-primary">Hozzáadás</button>
+		</form>
+
+	</div>

1e89944234fb10a84e7674268ed9c9f72369658b
Fizzor <marwel96@gmail.com>
2020-11-18 18:57:39 +0100

    Admin - felhasználók módosítása - backend

--- htdocs/protected/admin/userEdit.php
+++ htdocs/protected/admin/userEdit.php
@@ -1,3 +1,29 @@
+<?php
+require_once DATABASE_CONTROLLER;
+
+if(isset($_POST['edit'])):
+	$query = "UPDATE users SET username=:username, email=:email,flags=:flags WHERE id = :id";
+	$params = [ 
+	':username' => $_POST['username'],
+	':email' => $_POST['email'],
+	':flags' => $_POST['flags'],
+	':id' => $_POST['id']];
+	if(!executeDML($query,$params)):
+		echo 'Gebasz van<br>';
+		print_r($_POST);
+		echo '<br>';
+		print_r($params);
+	else:
+		header('Location: index.php?P=admin');
+	endif;
+
+else:
+	$query = "SELECT * FROM users WHERE id = :id";
+	$params = [':id' => $_GET['id']];
+	$d = getRecord($query,$params);
+	?>
+
+
 <div class="container-fluid p-4">
     <form method="POST">
         <input type="hidden" name="id" value="<?=$d['id']?>">
@@ -16,3 +42,4 @@
         <button name="edit" type="submit" class="btn btn-primary">Frissítés</button>
     </form>
 </div>
+<?php endif;?>

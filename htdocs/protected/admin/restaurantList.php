433be5f7682605ac9e2d215b187a973f0426db81
Fizzor <marwel96@gmail.com>
2020-11-18 19:56:01 +0100

    Frontend - étteremlista elkészítve

---
+++ htdocs/protected/admin/restaurantList.php
@@ -0,0 +1,28 @@
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Név</th>
+      <th scope="col">Szállítási idő</th>
+      <th scope="col">Szállítási díj</th>
+      <th scope="col">Értékelés összesen</th>
+      <th scope="col">Értékelők száma</th>
+      <th scope="col">Város id</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <tr>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td><a href="">Szerkesztés</a></td>
+      <td><a href="">Törlés</a></td>
+    </tr>
+
+  </tbody>
+</table>

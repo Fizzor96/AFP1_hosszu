e94d8edd55b2e8ca53a6feec5861fc93a2e5a57c
Fizzor <marwel96@gmail.com>
2020-11-18 20:19:36 +0100

    Backend - városok hozzáadása elkészítve

--- htdocs/protected/admin/addCity.php
+++ htdocs/protected/admin/addCity.php
@@ -1,4 +1,20 @@
-<div class="container-fluid p-4">
+<?php
+require_once DATABASE_CONTROLLER;
+
+if(isset($_POST['edit'])):
+	$query = "INSERT INTO cities (Name) VALUES (:Name)";
+	$params = [':Name' => $_POST['Name']];
+	if(!executeDML($query,$params)):
+		print_r($_POST);
+		echo '<br>';
+		print_r($params);
+	else:
+		header('Location: index.php?P=admin');
+	endif;
+
+else:
+	?>
+	<div class="container-fluid p-4">
 		<form method="POST">
 			<input type="hidden" name="id">
 			<div class="form-group">
@@ -7,4 +23,5 @@
 			</div>
 			<button name="edit" type="submit" class="btn btn-primary">Hozzáadás</button>
 		</form>
-</div>
+	</div>
+<?php endif;?>

54459827f21a5fbd0d077afb13e3a4af46dc18a0
Fizzor <marwel96@gmail.com>
2020-11-18 20:18:50 +0100

    Frontend - városok hozzáadása elkészítve

---
+++ htdocs/protected/admin/addCity.php
@@ -0,0 +1,10 @@
+<div class="container-fluid p-4">
+		<form method="POST">
+			<input type="hidden" name="id">
+			<div class="form-group">
+				<label >Város neve</label>
+				<input name="name" type="text" class="form-control">
+			</div>
+			<button name="edit" type="submit" class="btn btn-primary">Hozzáadás</button>
+		</form>
+</div>

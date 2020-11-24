050c0ad9f5c05b7d2bfe3f7dfd8b4062d3a5c5bc
Bence <egyjatekos0204@gmail.com>
2020-11-24 13:36:02 +0100

    Frontend: Rendelések megjelenítése éttermeknek
    Orders.php létrehozása.

---
+++ htdocs/protected/pages/orders.php
@@ -0,0 +1,52 @@
+
+<!--
+
+Aktuális-Rendelések:
+    id
+    userid
+    restaurantid
+    rendelesek
+    status
+
+
+
+-->
+
+
+
+
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Ügyfél</th>
+      <th scope="col">Rendelés(ek)</th>
+      <th scope="col">Elkészült</th>
+      <th scope="col">Törlés</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>József Attila</td>
+      <td>Túrós csúsza<br>Rántott hús</td>
+      <td><button class="btn btn-success  ">Elkészült</button></td>
+      <td><button class="btn btn-danger  ">Rendelés törlése</button></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>József Attila</td>
+      <td>Túrós csúsza<br>Rántott hús</td>
+      <td><button class="btn btn-success  ">Elkészült</button></td>
+      <td><button class="btn btn-danger  ">Rendelés törlése</button></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>József Attila</td>
+      <td>Túrós csúsza<br>Rántott hús</td>
+      <td><button class="btn btn-success  ">Elkészült</button></td>
+      <td><button class="btn btn-danger  ">Rendelés törlése</button></td>
+    </tr>
+  </tbody>
+</table>

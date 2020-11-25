7447421d7bd50a7de0dae688de6440fc5dcba8dd
Bence <egyjatekos0204@gmail.com>
2020-11-26 00:16:05 +0100

    Függőben lévő rendelések listázása
    Az éttermek látják a hozzájuk tartozó rendeléseket, az adminnal rendelkezők pedig minden rendelést.

--- htdocs/protected/pages/orders.php
+++ htdocs/protected/pages/orders.php
@@ -16,30 +16,61 @@ Aktuális-Rendelések:
 require_once DATABASE_CONTROLLER;
 
 if (getConnection()):
-    $query = "SELECT * FROM aktualis_rendelesek WHERE restaurantid = :restaurantid";
-    $params = [ ':restaurantid' => $_SESSION['userid']];
-    $datas = getList($query,$params);
-    var_dump($datas);
+    if($_SESSION['flags'] > 5){
+      $query = "SELECT * FROM rendelesek WHERE status = 0";
+      $params = [];
+    }
+    else {
+      $query = "SELECT * FROM rendelesek WHERE restaurantid = :restaurantid and status = 0";
+      $params = [ ':restaurantid' => $_SESSION['userid']];
+    }
+    
+    $rendelesek = getList($query,$params);
+
+    $usernames = [];
+
+    for ($i=0; $i < count($rendelesek); $i++) { 
+      $query = "SELECT username FROM users WHERE id = :id";
+      $params1 = [':id' => $rendelesek[$i]['userid']];
+      $username = getField($query, $params1) ? getField($query, $params1) : null;
+      $usernames[] = $username;
+    }
+    
+
+    //var_dump($rendelesek);
+    //var_dump($usernames);
 ?>
-<table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Ügyfél</th>
-      <th scope="col">Rendelés(ek)</th>
-      <th scope="col">&nbsp; </th>
-      <th scope="col">&nbsp;</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php for ($i=0; $i < count($datas); $i++):?>
-    <tr>
-      <th scope="row"><?=$i+1?></th>
-      <td><?=$datas[$i]['userid']?></td>
-      <td><?=$datas[$i]['rendelesek']?></td>
-      <td><button class="btn btn-success" href="#" >Elkészült</button></td>
-      <td><button class="btn btn-danger" href="#" >Rendelés törlése</button></td>
-    </tr>
-    <?php endfor;endif;?>
-  </tbody>
-</table>
+<div class="container mt-3">
+  <table class="table table-dark">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Ügyfél</th>
+        <th scope="col">Rendelés(ek)</th>
+        <th scope="col">&nbsp; </th>
+        <th scope="col">&nbsp;</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php for ($i=0; $i < count($rendelesek); $i++):?>
+      <tr>
+        <th scope="row"><?=$i+1?></th>
+        <td>
+        <?php
+        if($usernames[$i] != ""){
+          printf($usernames[$i]);
+        }
+        else {
+          printf('<span style="color: red;">Törölt felhasználó</span>');
+        }
+        ?>
+        
+        </td>
+        <td><?=$rendelesek[$i]['rendelesek']?></td>
+        <td><a class="btn btn-success" href="index.php?P=orderReady&id=<?=$rendelesek[$i]['id']?>" >Elkészült</a></td>
+        <td><a class="btn btn-danger" href="index.php?P=orderDelete&id=<?=$rendelesek[$i]['id']?>" >Rendelés törlése</a></td>
+      </tr>
+      <?php endfor;endif;?>
+    </tbody>
+  </table>
+</div>
